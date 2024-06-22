4c7f6fe88d6c1e6f07c6b0531723b67c782071c8
MRamirez202210582 <U202210582@upc.edu.pe>
2024-06-22 01:09:21 -0500

    Feature IAM files Application internal

---
+++ NexaLibery-Backend.API/IAM/Application/Internal/CommandServices/UserCommandService.cs
@@ -0,0 +1,61 @@
+﻿using NexaLibery_Backend.API.IAM.Application.Internal.OutboundServices;
+using NexaLibery_Backend.API.IAM.Domain.Model.Aggregates;
+using NexaLibery_Backend.API.IAM.Domain.Model.Commands;
+using NexaLibery_Backend.API.IAM.Domain.Repositories;
+using NexaLibery_Backend.API.IAM.Domain.Services;
+using NexaLibery_Backend.API.Shared.Domain.Repositories;
+
+namespace NexaLibery_Backend.API.IAM.Application.Internal.CommandServices
+{
+    public class UserCommandService(
+        IUserRepository userRepository,
+        ITokenService tokenService,
+        IHashingService hashingService,
+        IUnitOfWork unitOfWork)
+        : IUserCommandService
+    {
+        /**
+         * <summary>
+         *     Handle sign in command
+         * </summary>
+         * <param name="command">The sign in command</param>
+         * <returns>The authenticated user and the JWT token</returns>
+         */
+        public async Task<(User user, string token)> Handle(SignInCommand command)
+        {
+            var user = await userRepository.FindByEmailAsync(command.Email);
+
+            if (user == null || !hashingService.VerifyPassword(command.Password, user.PasswordHash))
+                throw new Exception("Invalid email or password");
+
+            var token = tokenService.GenerateToken(user);
+
+            return (user, token);
+        }
+
+        /**
+         * <summary>
+         *     Handle sign up command
+         * </summary>
+         * <param name="command">The sign up command</param>
+         * <returns>A confirmation message on successful creation.</returns>
+         */
+        public async Task Handle(SignUpCommand command)
+        {
+            if (userRepository.ExistsByUsername(command.Username))
+                throw new Exception($"Username {command.Username} is already taken");
+
+            var hashedPassword = hashingService.HashPassword(command.Password);
+            var user = new User(command.Username, hashedPassword, command.Email, command.Description, command.CardNumber, command.BornDate, command.Photo);
+            try
+            {
+                await userRepository.AddAsync(user);
+                await unitOfWork.CompleteAsync();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"An error occurred while creating user: {e.Message}");
+            }
+        }
+    }
+}
---
+++ NexaLibery-Backend.API/IAM/Application/Internal/QueryServices/UserQueryService.cs
@@ -0,0 +1,47 @@
+﻿using NexaLibery_Backend.API.IAM.Domain.Model.Aggregates;
+using NexaLibery_Backend.API.IAM.Domain.Model.Queries;
+using NexaLibery_Backend.API.IAM.Domain.Repositories;
+using NexaLibery_Backend.API.IAM.Domain.Services;
+
+namespace NexaLibery_Backend.API.IAM.Application.Internal.QueryServices;
+
+public class UserQueryService(IUserRepository userRepository) : IUserQueryService
+{
+    /**
+     * <summary>
+     *     Handle get user by id query
+     * </summary>
+     * <param name="query">The query object containing the user id to search</param>
+     * <returns>The user</returns>
+     */
+    public async Task<User?> Handle(GetUserByIdQuery query)
+    {
+        return await userRepository.FindByIdAsync(query.Id);
+    }
+
+    /**
+     * <summary>
+     *     Handle get all users query
+     * </summary>
+     * <param name="query">The query object for getting all users</param>
+     * <returns>A list of users</returns>
+     */
+    public async Task<IEnumerable<User>> Handle(GetAllUsersQuery query)
+    {
+        return await userRepository.ListAsync();
+    }
+
+    /**
+     * <summary>
+     *     Handle get user by email query
+     * </summary>
+     * <param name="query">The query object containing the email to search</param>
+     * <returns>The user</returns>
+     */
+    public async Task<User?> Handle(GetUserByEmailQuery query)
+    {
+        return await userRepository.FindByEmailAsync(query.Email);
+    }
+
+ 
+}
