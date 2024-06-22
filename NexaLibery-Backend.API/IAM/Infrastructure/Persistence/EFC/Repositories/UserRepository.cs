1c0c84f5434bce187067e1d8f0bebc1b56d8a890
MRamirez202210582 <U202210582@upc.edu.pe>
2024-06-22 01:20:02 -0500

    Feature infrastructure Hashing an persistence repository

---
+++ NexaLibery-Backend.API/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using NexaLibery_Backend.API.IAM.Domain.Model.Aggregates;
+using NexaLibery_Backend.API.IAM.Domain.Repositories;
+using NexaLibery_Backend.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using NexaLibery_Backend.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace NexaLibery_Backend.API.IAM.Infrastructure.Persistence.EFC.Repositories;
+
+public class UserRepository(AppDbContext context) : BaseRepository<User>(context), IUserRepository
+{
+    /**
+     * <summary>
+     *     Find a user by email
+     * </summary>
+     * <param name="email">The email to search</param>
+     * <returns>The user</returns>
+     */
+    public async Task<User?> FindByEmailAsync(string email)
+    {
+        return await Context.Set<User>().FirstOrDefaultAsync(user => user.Email.Equals(email));
+    }
+
+    /**
+     * <summary>
+     *     Find a user by username
+     * </summary>
+     * <param name="username">The username to search</param>
+     * <returns>The user</returns>
+     */
+    public async Task<User?> FindByUsernameAsync(string username)
+    {
+        return await Context.Set<User>().FirstOrDefaultAsync(user => user.Username.Equals(username));
+    }
+
+    /**
+     * <summary>
+     *     Check if a user exists by username
+     * </summary>
+     * <param name="username">The username to search</param>
+     * <returns>True if the user exists, false otherwise</returns>
+     */
+    public bool ExistsByUsername(string username)
+    {
+        return Context.Set<User>().Any(user => user.Username.Equals(username));
+    }
+}
